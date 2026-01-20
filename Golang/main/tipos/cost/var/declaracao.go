8c140acc3ccdf2fe1885c3607bfdbd68f0f32394
Rebeca <becalbuquerque_23@hotmail.com>
2026-01-20 18:33:34 -0300

    tipos de declaração de variavel

---
+++ Golang/main/tipos/cost/var/declaracao.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nome := "Rebeca" //declarando uma variavel do tipo string
+	fmt.Println(nome)
+
+	numero := 20 //declarando uma variavel do tipo int
+	fmt.Println(numero)
+
+	var sobrenome string //declarando uma variavel do tipo string
+	sobrenome = "guanaes"
+	fmt.Println(sobrenome)
+
+	sobrenome = "albuquerque" //acrescentando mais um valor a mesma variavel
+	fmt.Println(sobrenome)
+
+	altura := 1.65 //declarando uma variavel do tipo float
+	fmt.Println(altura)
+}
