50846fb2e8892c649d1fe03ec837aa0b84d85936
Rebeca <becalbuquerque_23@hotmail.com>
2026-01-28 19:35:52 -0300

    aplicando condicional

---
+++ Golang/main/tipos/cost/var/funcoes/array/maps/struct/if-else/if-else.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//valor := 3
+	//if valor == 1 {
+	//	fmt.Println("o valor é igual a 1")
+	//} else {
+	//	fmt.Println("O valor não é igual a 1")
+	//}
+	valor := 2 //declarando variavel
+	if valor == 1 {
+		fmt.Println("O valor é igual a 1")
+	} else if valor == 2 {
+		fmt.Println("O valor é igual a 2")
+	} else {
+		fmt.Println("O valor não é igual 1 e nem igual a 2")
+	}
+
+}
